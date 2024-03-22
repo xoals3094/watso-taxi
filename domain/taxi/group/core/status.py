ad8a51a4b4262cfbd5dc100705744e2699ef22e4
TaeMin <xoals3094@naver.com>
2024-03-22 16:05:40 +0900

    refactor: 상태에 따른 예외 발생 로직을 status enum 객체의 클래스로 이전

--- domain/taxi/group/core/status.py
+++ domain/taxi/group/core/status.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from exceptions import DomainException
 
 
 class Status(str, Enum):
@@ -8,26 +9,38 @@ class Status(str, Enum):
     SETTLEMENT = 'SETTLEMENT'
     COMPLETED = 'COMPLETED'
 
-    def modifiable(self):
-        if self in [Status.RECRUITING, Status.CLOSED]:
-            return True
-        return False
+    def modify(self):
+        if self not in [Status.RECRUITING, Status.CLOSED]:
+            raise DomainException.PostModificationFailedException(
+                msg=f"'{self}' 상태에서는 게시글의 수정이 불가능합니다."
+            )
 
-    def changeable(self, status):
-        if self is Status.RECRUITING:
-            return status in [Status.RECRUITING, Status.CLOSED]
+    def to(self, status):
+        if (self is Status.RECRUITING) and (status in [Status.RECRUITING, Status.CLOSED]):
+            return
 
-        elif self is Status.CLOSED:
-            return status in [Status.RECRUITING, Status.CLOSED, Status.BOARDING]
+        elif (self is Status.CLOSED) and (status in [Status.RECRUITING, Status.CLOSED, Status.BOARDING]):
+            return
 
-        elif self is Status.BOARDING:
-            return status is Status.SETTLEMENT
+        elif (self is Status.BOARDING) and (status is Status.SETTLEMENT):
+            return
 
-        elif self is Status.SETTLEMENT:
-            return status is Status.COMPLETED
+        elif (self is Status.SETTLEMENT) and (status is Status.COMPLETED):
+            return
 
-    def can_participate(self):
-        return self == Status.RECRUITING
+        raise DomainException.InvalidStateException(
+            msg=f"'{self}' -> '{status}'는 허용되지 않는 상태코드 변경입니다."
+        )
 
-    def can_leave(self):
-        return self == Status.RECRUITING
+    def participate(self):
+        if self != Status.RECRUITING:
+            raise DomainException.ParticipationFailedException(
+                msg=f"'{self}' 상태에서는 참여가 불가능합니다."
+            )
+
+    def leave(self):
+        if self != Status.RECRUITING:
+            raise DomainException.LeaveFailedException(
+                msg=f"'{self}' 상태에서는 탈퇴가 불가능합니다."
+            )
+

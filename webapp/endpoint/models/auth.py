d14428b86ea289c28a0bd7b66297918e357b81a6
TaeMin <xoals3094@naver.com>
2024-09-14 13:43:33 +0900

    fix: refresh 응답 모델 오탈자 수정
    access token이 refresh token으로 작성되었던 오탈자를 수정합니다

--- webapp/endpoint/models/auth.py
+++ webapp/endpoint/models/auth.py
@@ -2,15 +2,12 @@ from pydantic import BaseModel, Field
 
 
 class AccessToken(BaseModel):
-    refresh_token: str = Field(..., description='refresh token', examples=['jwt token'])
+    access_token: str = Field(..., description='access_token', examples=['jwt token'])
 
 
 class RefreshToken(BaseModel):
     refresh_token: str = Field(..., description='refresh token', examples=['jwt token'])
 
 
-class TokenPair(AccessToken, RefreshToken):
-    access_token: str = Field(..., description='access token', examples=['jwt token'])
-    refresh_token: str = Field(..., description='refresh token', examples=['jwt token'])
-
-
+class TokenPair(RefreshToken, AccessToken):
+    pass

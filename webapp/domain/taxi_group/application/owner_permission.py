0092159fefc29e199e28f4253871eeac52bf89da
TaeMin <xoals3094@naver.com>
2024-09-15 14:20:28 +0900

    feat: 그룹 owner 권한 체크 기능 구현

--- webapp/domain/taxi_group/application/owner_permission.py
+++ webapp/domain/taxi_group/application/owner_permission.py
@@ -1,18 +1,26 @@
 from fastapi import Depends
+from sqlalchemy import select
+from webapp.common.database import get_session
 from webapp.common.util.token_decoder import get_user_id
+from webapp.domain.taxi_group.entity.taxi_group import TaxiGroup
 
 from webapp.common.exceptions import auth
 
 
-def find_owner_id_by_group_id(group_id) -> str:
-    return '9762d71287d843d2b1213e2b66740c84'
+def find_owner_id_by_group_id(group_id: str, session=Depends(get_session)) -> str:
+    stmt = select(TaxiGroup.owner_id).where(
+        TaxiGroup.id == group_id
+    )
+
+    result = session.execute(stmt)
+    owner_id = result.scalar_one()
+    return owner_id
 
 
 def owner_permission(
-        group_id: str,
+        owner_id: str = Depends(find_owner_id_by_group_id),
         user_id: str = Depends(get_user_id)
 ):
-    owner_id = find_owner_id_by_group_id(group_id)
     if owner_id != user_id:
         raise auth.AccessDenied(msg='그룹 수정 권한이 없습니다')
     return

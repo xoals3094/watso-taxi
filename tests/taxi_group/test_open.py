6ceb137ebc4d929f158fe58752662c83e1b2525b
TaeMin <xoals3094@naver.com>
2024-09-27 10:28:22 +0900

    test: settle to open 실패 테스트 추가

--- tests/taxi_group/test_open.py
+++ tests/taxi_group/test_open.py
@@ -10,3 +10,11 @@ def test_success_close_to_open():
     taxi_group.open()
 
     assert taxi_group.status == Status.OPEN
+
+
+def test_fail_settle_to_open():
+    taxi_group = setup.from_json_to_entity(setup.taxi_group_settle)
+
+    with pytest.raises(domain.InvalidState) as e:
+        taxi_group.open()
+    assert str(e.value) == "'SETTLE' -> 'OPEN'는 허용되지 않는 상태코드 변경입니다."
